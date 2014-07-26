6fb587a059200a86674778ee620b7b7098e38b8f
Evgeny <liquid.dream@gmail.com>
2014-07-26 06:04:03 +0000

    Switching to Hogan and Less

--- app.js
+++ app.js
@@ -6,24 +6,27 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var routes = require('./routes/about');
+var about = require('./routes/about');
 var users = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/about', about);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
